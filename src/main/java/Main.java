8439306c7702a9fb26a17491daaf10a1f86e929b
JosephVuoto <joseph.vuoto@gmail.com>
2020-04-11 19:12:24 +1000

    configure logger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
+import org.apache.log4j.Logger;
+
 /**
  * @author Yangzhe Xie
  * @date 11/4/20
  */
 public class Main {
+    final static Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        logger.debug("Hello world");
     }
 }
