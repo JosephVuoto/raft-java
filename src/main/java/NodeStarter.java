3289d8d41c72a444f4d8d87fce4a2d2a6c07bcf1
Qifan <plocircle@live.com>
2020-05-15 10:41:31 +0800

    format: please user ./format.sh before commit

--- src/main/java/NodeStarter.java
+++ src/main/java/NodeStarter.java
@@ -7,20 +7,20 @@ import java.rmi.RemoteException;
  * Starter class to start a node
  */
 public class NodeStarter {
-    public static void main(String[] args) {
-        //TODO: start a node:
-        //1. read serverId, registry address from a config file
-        //2. create a NodeImpl
-        //3. register the remote object
-        //4. set a state (follower) to the node, then start it
-        int serverId = 0;
-        String url = ".....";
-        try {
-            NodeImpl node = new NodeImpl(serverId);
-            Naming.bind(url, node);
-            node.setState(new FollowerState(node));
-        } catch (RemoteException | MalformedURLException | AlreadyBoundException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		// TODO: start a node:
+		// 1. read serverId, registry address from a config file
+		// 2. create a NodeImpl
+		// 3. register the remote object
+		// 4. set a state (follower) to the node, then start it
+		int serverId = 0;
+		String url = ".....";
+		try {
+			NodeImpl node = new NodeImpl(serverId);
+			Naming.bind(url, node);
+			node.setState(new FollowerState(node));
+		} catch (RemoteException | MalformedURLException | AlreadyBoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
