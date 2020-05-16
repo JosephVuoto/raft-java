b78d00435946c1fe2406a6a4b4cca14ddeb4b70a
Alan Ung <alanu@student.unimelb.edu.au>
2020-05-17 01:28:51 +1000

    Add methods and exception classes to RaftLog

--- src/main/java/LogEntry.java
+++ src/main/java/LogEntry.java
@@ -1,5 +1,40 @@
-public class LogEntry {
+import java.io.Serializable;
+
+public class LogEntry<T> implements Serializable {
 	private int index;
 	private int term;
-	// TODO: define the data structure of a log entry
+	private T command;
+	private boolean committed = false;
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public int getTerm() {
+		return term;
+	}
+
+	public void setTerm(int term) {
+		this.term = term;
+	}
+
+	public T getCommand() {
+		return command;
+	}
+
+	public void setCommand(T command) {
+		this.command = command;
+	}
+
+	public boolean isCommitted() {
+		return committed;
+	}
+
+	public void setCommitted(boolean committed) {
+		this.committed = committed;
+	}
 }
