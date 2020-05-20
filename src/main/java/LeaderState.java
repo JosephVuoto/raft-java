5d07e3a09af67c734ed0ba7119d1381f850a0bab
Alan Ung <alanu@student.unimelb.edu.au>
2020-05-20 21:12:25 +1000

    Remove unused imports

--- src/main/java/LeaderState.java
+++ src/main/java/LeaderState.java
@@ -1,7 +1,5 @@
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
