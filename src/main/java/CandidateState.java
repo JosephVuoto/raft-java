bbf1eeb1a1571bdb87e51cedb3872af4becb52ee
Qifan <plocircle@live.com>
2020-05-20 22:13:51 +0800

    candidate: idk

--- src/main/java/CandidateState.java
+++ src/main/java/CandidateState.java
@@ -1,8 +1,6 @@
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.*;
 
 public class CandidateState extends AbstractState {
@@ -11,36 +9,15 @@ public class CandidateState extends AbstractState {
 	protected boolean isWaitingForVoteResponse;
 	protected int myVotes;
 
-	private ScheduledExecutorService scheduledExecutorService;
-
-	private final Map<INode, Integer> nextIndex;
-	/* for each server, index of highest log entry known to be replicated on server (initialized to 0, increases
-	 * monotonically). The data structure is <node id, index of highest log entry...>. Reinitialized after election */
-	private final Map<INode, Integer> matchIndex;
-
 	public CandidateState(NodeImpl node) {
 		super(node);
-		// set majority threshold
 		MAJORITY_THRESHOLD = (node.getRemoteNodes().size() + 1) / 2 + 1;
 		isWaitingForVoteResponse = false;
 		myVotes = 0;
-		nextIndex = new HashMap<>();
-		matchIndex = new HashMap<>();
 	}
 
-	/**
-	 * Initialize attributes and setup a kind of
-	 * timeout timer for election and start it
-	 */
 	public void start() {
-		// TODO: initialize the candidate state
 		scheduleAnotherRound();
-
-		for (INode remoteNode : node.getRemoteNodes()) {
-			nextIndex.put(remoteNode, node.getRaftLog().getLastEntryIndex() + 1);
-			matchIndex.put(remoteNode, 0);
-		}
-
 		requestAllRemoteNodesToVote();
 	}
 
@@ -51,14 +28,6 @@ public class CandidateState extends AbstractState {
 	 */
 	public VoteResponse requestVote(int remoteTerm, int remoteCandidateId, int remoteLastLogIndex,
 	                                int remoteLastLogTerm) {
-		// TODO: determine whether to vote for the caller. if yes, then the node should
-		// go back to follower state, if not, return the current term number.
-
-		if (haveAMajorityVotes || haveVotedOtherNode) {
-			// if I am in a transform progress
-			return new VoteResponse(false, currentTerm);
-		}
-
 		if (!isWaitingForVoteResponse) {
 			// if I am not waiting my votes
 			if (remoteTerm < currentTerm || remoteLastLogIndex < commitIndex) {
@@ -67,8 +36,7 @@ public class CandidateState extends AbstractState {
 				return new VoteResponse(false, currentTerm);
 			} else {
 				// otherwise  be ready to become a follower and vote the caller
-				currentTerm = remoteTerm;
-				becomeFollower(remoteCandidateId);
+				becomeFollower(remoteTerm, remoteCandidateId);
 				return new VoteResponse(true, currentTerm);
 			}
 		} else {
@@ -76,8 +44,7 @@ public class CandidateState extends AbstractState {
 			if (remoteTerm > currentTerm || remoteLastLogIndex > commitIndex) {
 				// if the remote caller has a greater term or it has a news log
 				// be ready to become a follower and vote the caller
-				currentTerm = remoteTerm;
-				becomeFollower(remoteCandidateId);
+				becomeFollower(remoteTerm, remoteCandidateId);
 				return new VoteResponse(true, currentTerm);
 			} else {
 				// otherwise reject
@@ -93,40 +60,37 @@ public class CandidateState extends AbstractState {
 	 */
 	public AppendResponse appendEntries(int remoteTerm, int remoteLeaderId, int remotePrevLogIndex,
 	                                    int remotePrevLogTerm, LogEntry[] remoteEntries, int remoteLeaderCommit) {
-		// TODO: if receive this RPC with a higher term number, then go back to follower
-		// state. if not, return the current term number.
 
 		int myLastLogIndex = this.node.getRaftLog().getLastEntryIndex();
 		int myLastLogTerm = this.node.getRaftLog().getTermOfEntry(myLastLogIndex);
 		// WIP: confusing part, need to discuss
 		if (remoteTerm >= currentTerm || remotePrevLogTerm >= myLastLogTerm || remotePrevLogIndex >= myLastLogIndex) {
 			// if remote caller has a greater term and last log entry
-			currentTerm = remoteTerm;
-			becomeFollower(remoteLeaderId);
+			setCurrentTerm(remoteTerm) becomeFollower(remoteLeaderId);
 		}
-		return doAppendEntries(remoteTerm, remoteLeaderId, remotePrevLogIndex, remotePrevLogTerm, remoteEntries,
-		                       remoteLeaderCommit);
+		return doAppendEntries(remoteTerm, remotePrevLogIndex, remotePrevLogTerm, remoteEntries, remoteLeaderCommit);
 	}
 
-	private AppendResponse doAppendEntries(int remoteTerm, int remoteLeaderId, int remotePrevLogIndex,
-	                                       int remotePrevLogTerm, LogEntry[] remoteEntries, int remoteLeaderCommit) {
-
-		// Update term
-		currentTerm = remoteTerm;
+	private AppendResponse doAppendEntries(int remoteTerm, int remotePrevLogIndex, int remotePrevLogTerm,
+	                                       LogEntry[] remoteEntries, int remoteLeaderCommit) {
 
 		// 1. Reply false if term < currentTerm (§5.1)
-		if (remoteTerm < currentTerm)
+		if (remoteTerm < currentTerm) {
+			node = null;
 			return new AppendResponse(false, currentTerm);
+		}
 		// 2. Reply false if log doesn’t contain an entry at prevLogIndex
 		//    whose term matches prevLogTerm (§5.3)
-		if (node.getRaftLog().getTermOfEntry(remotePrevLogIndex) != remotePrevLogTerm)
+		if (node.getRaftLog().getTermOfEntry(remotePrevLogIndex) != remotePrevLogTerm) {
 			return new AppendResponse(false, currentTerm);
+		}
 		// 3. If an existing entry conflicts with a new one (same index
 		//	  but different terms), delete the existing entry and all that
 		//    follow it (§5.3)
 		// 4. Append any new entries not already in the log
 		try {
-			node.getRaftLog().writeEntries(remotePrevLogIndex, new ArrayList<LogEntry>(Arrays.asList(remoteEntries)));
+			node.getRaftLog().writeEntries(remotePrevLogIndex, new ArrayList<>(Arrays.asList(remoteEntries)));
+			writePersistentState();
 		} catch (RaftLog.MissingEntriesException e) {
 			System.out.println("Entries missing: " + e);
 		} catch (RaftLog.OverwriteCommittedEntryException e) {
@@ -134,8 +98,15 @@ public class CandidateState extends AbstractState {
 		}
 		// 5. If leaderCommit > commitIndex, set commitIndex =
 		// min(leaderCommit, index of last new entry)
-		if (remoteLeaderCommit > commitIndex)
+		if (remoteLeaderCommit > commitIndex) {
 			commitIndex = Math.min(remoteLeaderCommit, node.getRaftLog().getLastEntryIndex());
+			try {
+				node.getRaftLog().commitToIndex(commitIndex);
+			} catch (RaftLog.MissingEntriesException e) {
+				e.printStackTrace();
+			}
+		}
+		node = null;
 		return new AppendResponse(true, currentTerm);
 	}
 
@@ -174,6 +145,7 @@ public class CandidateState extends AbstractState {
 	}
 
 	private void scheduleAnotherRound() {
+		ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 		scheduledExecutorService.schedule(() -> {
 			if (node != null) {
 				currentTerm += 1;
@@ -184,12 +156,14 @@ public class CandidateState extends AbstractState {
 	}
 
 	private void becomeLeader() {
-		votedFor = this.node.getNodeId();
+		setVoteFor(this.node.getNodeId());
 		node.setState(new LeaderState(node));
 		this.node = null;
 	}
 
-	private void becomeFollower(int iVotedFor) {
+	private void becomeFollower(int remoteTerm, int iVotedFor) {
+		setCurrentTerm(remoteTerm);
+
 		node.setState(new FollowerState(node, iVotedFor));
 		this.node = null;
 	}
@@ -202,10 +176,9 @@ public class CandidateState extends AbstractState {
 	/**
 	 * If a client contacts a follower, the follower redirects it to the leader
 	 *
-	 * @return
+	 * @return the res of the command sending
 	 */
 	private String sendToLeader(String command, int timeout) {
-		// TODO: Currently I don't know when I would get a command;
 		String res = "Fail to write the log";
 		try {
 			INode leader = node.getRemoteNodes().get(votedFor);
@@ -216,5 +189,23 @@ public class CandidateState extends AbstractState {
 			System.out.println("Cannot reach the remote node: " + e);
 		}
 		return res;
+	}
+
+	private void setVoteFor(int newVotedFor) {
+		votedFor = newVotedFor;
+		writePersistentState();
+	}
+
+	/**
+	 * Ser currentTerm and store it persistently.
+	 *
+	 * @param term term num
+	 */
+	private void setCurrentTerm(int term) {
+		if (term > currentTerm) {
+			votedFor = -1;
+			currentTerm = term;
+			writePersistentState();
+		}
 	}
 }
