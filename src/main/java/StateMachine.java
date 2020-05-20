ab9d1f7cc884481076641d86d7bc8b943727f0aa
JosephVuoto <joseph.vuoto@gmail.com>
2020-05-20 16:32:48 +1000

    Fix issues in LogEntry

--- src/main/java/StateMachine.java
+++ src/main/java/StateMachine.java
@@ -1,8 +1,38 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
- * @author Yangzhe Xie
- * @date 20/5/20
+ * The state machine, which is a key-value database
  */
 public class StateMachine {
+	private Map<String, String> db = new HashMap<>();
+
+	/**
+	 * set command. e.g. [set name joseph]
+	 * @param key key
+	 * @param value value
+	 * @return result
+	 */
+	public String set(String key, String value) {
+		db.put(key, value);
+		return "OK";
+	}
+
+	/**
+	 * get command. e.g. [get name]
+	 * @param key key
+	 * @return stored value, "null" if no vaule is found
+	 */
+	public String get(String key) {
+		return db.getOrDefault(key, "null");
+	}
+
+	/**
+	 * get all keys in the db. e.g. [keys]
+	 * @return key set
+	 */
+	public Set<String> keys() {
+		return db.keySet();
+	}
 }
